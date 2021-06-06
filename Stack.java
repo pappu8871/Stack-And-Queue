8fdd0711fe5bf9105c1675c101c217b9ec397be2
your username <59005110+pappu8871@users.noreply.github.com>
2021-06-06 10:06:34 +0530

    Added:pop And peek operation

--- Stack.java
+++ Stack.java
@@ -1,13 +1,13 @@
 package com.stack;
 
-public class StackUC1 {
-
+public class StackUC2 {
 	class Node {
 		int data;
 		Node link;
+
 	}
 	Node top;
-	StackUC1() {
+	StackUC2() {
 		this.top =null;
 	}
 	public void push (int data) {
@@ -17,6 +17,26 @@ public class StackUC1 {
 		top = temp;
 	}
 
+	public boolean isEmpty()
+	{
+		return top == null;
+	}
+
+	public int peek() {
+		if (!isEmpty()) {
+			return top.data;
+		} else {
+			return 1 ;
+		}
+	}
+
+	public void pop() {
+		if (top == null) {
+			return;
+		}
+		top = (top).link;
+	}
+
 	public void display() {
 		if (top == null) {
 			return;
@@ -24,7 +44,7 @@ public class StackUC1 {
 		else {
 			Node temp = top;
 			while (temp != null) {
-				
+
 				System.out.print( " " + temp.data);
 				temp = temp.link;
 			}
@@ -32,13 +52,24 @@ public class StackUC1 {
 	}
 
 	public static void main(String[] args){
-		StackUC1 list = new StackUC1();
+		StackUC2 list = new StackUC2();
 
 		list.push(70);	
 		list.push(30);
 		list.push(56);
-         
+
 		System.out.println("Printed Stack list");
 		list.display();
+		System.out.println();
+
+		list.pop();
+		System.out.println("After pop data is dipaly");
+		list.display();
+		System.out.println();
+
+		System.out.println("peek of data ");
+		list.peek();
+		list.display();
+
 	}
 }
